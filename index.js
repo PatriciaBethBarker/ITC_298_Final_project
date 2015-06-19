f07b4dbdbed5efffb5f7db5becdc29ebc148b5d0
Patricia Beth Barker <patriciabethbarker@gmail.com>
2015-06-18 19:13:08 -0700

    finish uploading new instance

---
+++ index.js
@@ -0,0 +1,43 @@
+//index.js
+
+var hapi = require("hapi");
+//move the db require below server connection
+var server = new hapi.Server( {
+  //add connection settings, remove trailing slash in url
+  connections: {
+    router: {
+      //isCaseSensitive: true,
+      stripTrailingSlash: false
+    }
+  }
+});
+
+// Start the server
+server.connection({ port: 8000 });//listen using the connection function
+var db = require("./db");
+  db.init(function(err) { //this is the ready function
+  //if error statement
+    if (err) {
+      return console.error("db err", err);
+    }
+    // console.log("Database ready, start server.");
+    server.start(function() {
+      console.log("Server Ready");
+    });
+});
+
+server.views({
+//register the templates
+  engines: {
+    html: require("handlebars")
+  },
+  path: "views/templates", //"templates"
+  layoutPath: "views",
+  layout: "default",
+  //add partials path here
+    partialsPath: "views/templates/partials",
+  isCached: false
+});
+//register the routes, once matched, I want a response
+
+server.route(require("./routes"));
---
+++ routes.js
@@ -0,0 +1,43 @@
+//routes.js
+module.exports = [{
+//build the routes
+  path: "/",
+  method: "GET",
+  handler: require("./handlers/home")
+}, {
+  path: "/posts", //duplicate of home path
+  method: "GET",
+  handler: require("./handlers/home")
+}, {
+  path: "/posts/new",//new post
+  method: "GET",
+  handler: require("./handlers/addPost")
+}, {
+  path: "/posts/{slug}/edit",//edit post
+  method: "GET",
+  handler: require("./handlers/editPost")
+}, {
+  path: "/posts/{slug}",//save post
+  method: "POST",//post saved
+  handler: require("./handlers/savePost")
+}, {//create single page view
+  path: "/posts/{slug}",//return name of post
+  method: "GET",
+  handler: require("./handlers/viewPost")
+}, {
+  path: "/login",//login
+  method: "GET",
+  handler: require("./handlers/getLogin")
+}, {
+  path: "/login",
+  method: "POST",
+  handler: require("./handlers/postLogin")
+}, {
+  path: "/assets/{param*}",
+  method: "GET",
+  handler: {
+    directory: {
+      path: "./build"//changed from public
+    }
+  }
+}];
